fe05f41b487375f8a4b00c2c54a27c0377134bfe
ru-ab <ruabdrakhmanov@gmail.com>
2023-11-09 15:18:16 +0600

    refactor: change lines indent

--- scripts/app.js
+++ scripts/app.js
@@ -1,249 +1,249 @@
-"use strict";
+'use strict'
 
-const LOCAL_STORAGE_KEY = "HABIT_KEY";
-let habits = [];
-let globalActiveHabitId;
+const LOCAL_STORAGE_KEY = 'HABIT_KEY'
+let habits = []
+let globalActiveHabitId
 
 /* page */
 const page = {
-  menu: document.querySelector(".menu__list"),
-  header: {
-    h1: document.querySelector("h1"),
-    progress: document.querySelector(".progress"),
-    progressPercent: document.querySelector(".progress__percent"),
-    progressCoverBar: document.querySelector(".progress__cover-bar"),
-  },
-  main: {
-    days: document.querySelector(".days"),
-    addDay: document.querySelector(".habit"),
-    habitDayAdd: document.querySelector(".habit__day-add"),
-  },
-  popup: {
-    index: document.getElementById("add-habit-popup"),
-    iconField: document.querySelector(".popup__form input[name='icon']"),
-  },
-};
+	menu: document.querySelector('.menu__list'),
+	header: {
+		h1: document.querySelector('h1'),
+		progress: document.querySelector('.progress'),
+		progressPercent: document.querySelector('.progress__percent'),
+		progressCoverBar: document.querySelector('.progress__cover-bar'),
+	},
+	main: {
+		days: document.querySelector('.days'),
+		addDay: document.querySelector('.habit'),
+		habitDayAdd: document.querySelector('.habit__day-add'),
+	},
+	popup: {
+		index: document.getElementById('add-habit-popup'),
+		iconField: document.querySelector(".popup__form input[name='icon']"),
+	},
+}
 
 /* utils */
 function loadData() {
-  const habitsString = localStorage.getItem(LOCAL_STORAGE_KEY);
-  const habitArray = JSON.parse(habitsString);
+	const habitsString = localStorage.getItem(LOCAL_STORAGE_KEY)
+	const habitArray = JSON.parse(habitsString)
 
-  if (Array.isArray(habitArray)) {
-    habits = habitArray;
-  }
+	if (Array.isArray(habitArray)) {
+		habits = habitArray
+	}
 }
 
 function saveData() {
-  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(habits));
+	localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(habits))
 }
 
 /* render */
 function rerenderMenu(activeHabit) {
-  for (const habit of habits) {
-    const existed = document.querySelector(`[menu-habit-id="${habit.id}"]`);
-    if (!existed) {
-      const element = document.createElement("button");
-      element.addEventListener("click", () => rerender(habit.id));
-      element.setAttribute("menu-habit-id", habit.id);
-      element.classList.add("menu__item");
-      element.innerHTML = `<img src="./images/${habit.icon}.svg" alt="${habit.name}" />`;
-      if (habit.id === activeHabit.id) {
-        element.classList.add("menu__item_active");
-      }
-      page.menu.appendChild(element);
-      continue;
-    }
+	for (const habit of habits) {
+		const existed = document.querySelector(`[menu-habit-id="${habit.id}"]`)
+		if (!existed) {
+			const element = document.createElement('button')
+			element.addEventListener('click', () => rerender(habit.id))
+			element.setAttribute('menu-habit-id', habit.id)
+			element.classList.add('menu__item')
+			element.innerHTML = `<img src="./images/${habit.icon}.svg" alt="${habit.name}" />`
+			if (habit.id === activeHabit.id) {
+				element.classList.add('menu__item_active')
+			}
+			page.menu.appendChild(element)
+			continue
+		}
 
-    if (habit.id === activeHabit.id) {
-      existed.classList.add("menu__item_active");
-    } else {
-      existed.classList.remove("menu__item_active");
-    }
-  }
+		if (habit.id === activeHabit.id) {
+			existed.classList.add('menu__item_active')
+		} else {
+			existed.classList.remove('menu__item_active')
+		}
+	}
 }
 
 function rerenderHeader(activeHabit) {
-  page.header.progress.classList.remove("hidden");
+	page.header.progress.classList.remove('hidden')
 
-  page.header.h1.innerText = activeHabit.name;
+	page.header.h1.innerText = activeHabit.name
 
-  const progress =
-    activeHabit.days.length / activeHabit.target > 1
-      ? 100
-      : (activeHabit.days.length / activeHabit.target) * 100;
+	const progress =
+		activeHabit.days.length / activeHabit.target > 1
+			? 100
+			: (activeHabit.days.length / activeHabit.target) * 100
 
-  page.header.progressPercent.innerText = `${progress.toFixed(0)}%`;
-  page.header.progressCoverBar.setAttribute("style", `width: ${progress}%`);
+	page.header.progressPercent.innerText = `${progress.toFixed(0)}%`
+	page.header.progressCoverBar.setAttribute('style', `width: ${progress}%`)
 }
 
 function rerenderBody(activeHabit) {
-  page.main.addDay.classList.remove("hidden");
+	page.main.addDay.classList.remove('hidden')
 
-  page.main.days.innerHTML = "";
-  for (const [i, day] of activeHabit.days.entries()) {
-    const divHabit = document.createElement("div");
-    divHabit.classList.add("habit");
+	page.main.days.innerHTML = ''
+	for (const [i, day] of activeHabit.days.entries()) {
+		const divHabit = document.createElement('div')
+		divHabit.classList.add('habit')
 
-    const divHabitDay = document.createElement("div");
-    divHabitDay.classList.add("habit__day");
-    divHabitDay.innerText = `Day ${i + 1}`;
-    divHabit.appendChild(divHabitDay);
+		const divHabitDay = document.createElement('div')
+		divHabitDay.classList.add('habit__day')
+		divHabitDay.innerText = `Day ${i + 1}`
+		divHabit.appendChild(divHabitDay)
 
-    const divHabitComment = document.createElement("div");
-    divHabitComment.classList.add("habit__comment");
-    divHabitComment.innerText = day.comment;
-    divHabit.appendChild(divHabitComment);
+		const divHabitComment = document.createElement('div')
+		divHabitComment.classList.add('habit__comment')
+		divHabitComment.innerText = day.comment
+		divHabit.appendChild(divHabitComment)
 
-    const deleteButton = document.createElement("button");
-    deleteButton.addEventListener("click", () => deleteDay(i));
-    deleteButton.classList.add("habit__delete");
-    deleteButton.innerHTML = `<img src="images/delete.svg" alt="Delete day ${
-      i + 1
-    }" />`;
-    divHabit.appendChild(deleteButton);
+		const deleteButton = document.createElement('button')
+		deleteButton.addEventListener('click', () => deleteDay(i))
+		deleteButton.classList.add('habit__delete')
+		deleteButton.innerHTML = `<img src="images/delete.svg" alt="Delete day ${
+			i + 1
+		}" />`
+		divHabit.appendChild(deleteButton)
 
-    page.main.days.appendChild(divHabit);
-  }
+		page.main.days.appendChild(divHabit)
+	}
 
-  page.main.habitDayAdd.innerText = `Day ${activeHabit.days.length + 1}`;
+	page.main.habitDayAdd.innerText = `Day ${activeHabit.days.length + 1}`
 }
 
 function rerender(activeHabitId) {
-  globalActiveHabitId = activeHabitId;
-  const activeHabit = habits.find((habit) => habit.id === activeHabitId);
-  if (!activeHabit) {
-    return;
-  }
+	globalActiveHabitId = activeHabitId
+	const activeHabit = habits.find((habit) => habit.id === activeHabitId)
+	if (!activeHabit) {
+		return
+	}
 
-  document.location.replace(document.location.pathname + "#" + activeHabitId);
+	document.location.replace(document.location.pathname + '#' + activeHabitId)
 
-  rerenderMenu(activeHabit);
-  rerenderHeader(activeHabit);
-  rerenderBody(activeHabit);
+	rerenderMenu(activeHabit)
+	rerenderHeader(activeHabit)
+	rerenderBody(activeHabit)
 }
 
 /* work with days */
 function addDays(event) {
-  event.preventDefault();
-  const form = event.target;
-  const formValues = validateForm(form, ["comment"]);
-  if (!formValues) {
-    return;
-  }
+	event.preventDefault()
+	const form = event.target
+	const formValues = validateForm(form, ['comment'])
+	if (!formValues) {
+		return
+	}
 
-  habits = habits.map((habit) => {
-    if (habit.id === globalActiveHabitId) {
-      return {
-        ...habit,
-        days: habit.days.concat([{ comment: formValues.comment }]),
-      };
-    }
-    return habit;
-  });
-  resetForm(form, ["comment"]);
-  rerender(globalActiveHabitId);
-  saveData();
+	habits = habits.map((habit) => {
+		if (habit.id === globalActiveHabitId) {
+			return {
+				...habit,
+				days: habit.days.concat([{ comment: formValues.comment }]),
+			}
+		}
+		return habit
+	})
+	resetForm(form, ['comment'])
+	rerender(globalActiveHabitId)
+	saveData()
 }
 
 function deleteDay(index) {
-  habits = habits.map((habit) => {
-    if (habit.id === globalActiveHabitId) {
-      habit.days = habit.days.filter((day, i) => i !== index);
-    }
-    return habit;
-  });
+	habits = habits.map((habit) => {
+		if (habit.id === globalActiveHabitId) {
+			habit.days = habit.days.filter((day, i) => i !== index)
+		}
+		return habit
+	})
 
-  rerender(globalActiveHabitId);
-  saveData();
+	rerender(globalActiveHabitId)
+	saveData()
 }
 
 /* work with popup */
 function togglePopup() {
-  if (page.popup.index.classList.contains("cover_hidden")) {
-    page.popup.index.classList.remove("cover_hidden");
-  } else {
-    page.popup.index.classList.add("cover_hidden");
-  }
+	if (page.popup.index.classList.contains('cover_hidden')) {
+		page.popup.index.classList.remove('cover_hidden')
+	} else {
+		page.popup.index.classList.add('cover_hidden')
+	}
 }
 
 /* working with habits */
 function setIcon(context, icon) {
-  page.popup.iconField.value = icon;
-  const activeIcon = document.querySelector(".icon.icon_active");
-  activeIcon.classList.remove("icon_active");
-  context.classList.add("icon_active");
+	page.popup.iconField.value = icon
+	const activeIcon = document.querySelector('.icon.icon_active')
+	activeIcon.classList.remove('icon_active')
+	context.classList.add('icon_active')
 }
 
 function addHabit(event) {
-  event.preventDefault();
-  const form = event.target;
-  const formValues = validateForm(form, ["icon", "name", "target"]);
-  if (!formValues) {
-    return;
-  }
+	event.preventDefault()
+	const form = event.target
+	const formValues = validateForm(form, ['icon', 'name', 'target'])
+	if (!formValues) {
+		return
+	}
 
-  const maxId = habits.reduce(
-    (acc, habit) => (habit.id > acc ? habit.id : acc),
-    0
-  );
-  habits.push({
-    id: maxId + 1,
-    icon: formValues.icon,
-    name: formValues.name,
-    target: formValues.target,
-    days: [],
-  });
+	const maxId = habits.reduce(
+		(acc, habit) => (habit.id > acc ? habit.id : acc),
+		0,
+	)
+	habits.push({
+		id: maxId + 1,
+		icon: formValues.icon,
+		name: formValues.name,
+		target: formValues.target,
+		days: [],
+	})
 
-  saveData();
-  rerender(habits.length);
-  togglePopup();
-  resetForm(form, ["name", "target"]);
+	saveData()
+	rerender(habits.length)
+	togglePopup()
+	resetForm(form, ['name', 'target'])
 }
 
 function resetForm(form, fields) {
-  for (const field of fields) {
-    form[field].value = "";
-  }
+	for (const field of fields) {
+		form[field].value = ''
+	}
 }
 
 function validateForm(form, fields) {
-  const data = new FormData(form);
+	const data = new FormData(form)
 
-  const res = {};
-  for (const field of fields) {
-    const value = data.get(field);
-    form[field].classList.remove("error");
-    if (!value) {
-      form[field].classList.add("error");
-    }
-    res[field] = value;
-  }
+	const res = {}
+	for (const field of fields) {
+		const value = data.get(field)
+		form[field].classList.remove('error')
+		if (!value) {
+			form[field].classList.add('error')
+		}
+		res[field] = value
+	}
 
-  let isValid = true;
-  for (const field of fields) {
-    if (!res[field]) {
-      isValid = false;
-      break;
-    }
-  }
+	let isValid = true
+	for (const field of fields) {
+		if (!res[field]) {
+			isValid = false
+			break
+		}
+	}
 
-  if (!isValid) {
-    return;
-  }
+	if (!isValid) {
+		return
+	}
 
-  return res;
+	return res
 }
 
 /* init */
-(() => {
-  loadData();
+;(() => {
+	loadData()
 
-  const hashId = Number(document.location.hash.replace("#", ""));
-  const habit = habits.find((habit) => habit.id === hashId);
-  if (habit) {
-    rerender(habit.id);
-  } else {
-    rerender(habits[0]?.id);
-  }
-})();
+	const hashId = Number(document.location.hash.replace('#', ''))
+	const habit = habits.find((habit) => habit.id === hashId)
+	if (habit) {
+		rerender(habit.id)
+	} else {
+		rerender(habits[0]?.id)
+	}
+})()
